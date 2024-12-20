a4d6939195d6789589e9e707a0fc69811554f31f
paulomarchon <paulomarchon@gmail.com>
2024-12-20 15:12:39 -0300

    feat: Addition of the base project of the Account service

---
+++ backend/services/payment-processor/account/src/test/java/com/payments/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
